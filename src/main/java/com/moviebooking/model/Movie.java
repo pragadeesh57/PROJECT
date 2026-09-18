3c20b7d2c0cd53cd069ac52b82cedafd718616d6
pragadeesh57 <belivertamil57@gmail.com>
2026-09-18 22:57:17 +0530

    Create Movie.java

---
+++ src/main/java/com/moviebooking/model/Movie.java
@@ -0,0 +1,75 @@
+package com.moviebooking.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "movies")
+public class Movie {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String language;
+    private String genre;
+    private int duration;
+    private double price;
+
+    public Movie() {
+    }
+
+    public Movie(String title, String language,
+                 String genre, int duration, double price) {
+
+        this.title = title;
+        this.language = language;
+        this.genre = genre;
+        this.duration = duration;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
